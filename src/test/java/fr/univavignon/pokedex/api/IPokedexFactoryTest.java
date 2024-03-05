5a22de2bb1e8fe1f17bb2daa39335ebfa527524f
welmansouri <wafae.el-mansouri@alumni.univ-avignon.fr>
2024-03-05 11:16:00 +0100

    Test Failed : testCreatePokedex

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -0,0 +1,33 @@
+package fr.univavignon.pokedex.api;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
+public class IPokedexFactoryTest {
+
+    private IPokedexFactory pokedexFactory;
+    private IPokemonMetadataProvider metadataProvider;
+    private IPokemonFactory pokemonFactory;
+    @BeforeAll
+    public void setUp() {
+        this.pokedexFactory = mock(IPokedexFactory.class);
+        this.metadataProvider = mock(IPokemonMetadataProvider.class);
+        this.pokemonFactory = mock(IPokemonFactory.class);
+    }
+    @Test
+    public void testCreatePokedex() {
+        when(this.pokedexFactory.createPokedex(this.metadataProvider, this.pokemonFactory)).
+                thenReturn(mock(IPokedex.class));
+        IPokedex iPokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+        assertEquals(null, iPokedex);
+    }
+}
