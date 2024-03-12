4c6bda908aa5c97f6a90ca2f3d81b61d69a8601b
welmansouri <wafae.el-mansouri@alumni.univ-avignon.fr>
2024-03-12 12:53:36 +0100

    Test Succes : IPokedexTest(testPokemonsSortedByName)

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.when;
 
 public class IPokedexFactoryTest {
 
+
     private IPokedexFactory pokedexFactory;
     private IPokemonMetadataProvider metadataProvider;
     private IPokemonFactory pokemonFactory;
