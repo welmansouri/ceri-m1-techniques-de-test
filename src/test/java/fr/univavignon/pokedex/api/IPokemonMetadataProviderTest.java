7cca32c95dfa5134dd87158a835dfe37b9e65d03
welmansouri <wafae.el-mansouri@alumni.univ-avignon.fr>
2024-03-16 00:04:24 +0100

    Test Succes : IPokemonMetadataProviderTest

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -15,7 +15,7 @@ public class IPokemonMetadataProviderTest {
 
     @BeforeAll
     public void setUp() {
-        metadataProvider = mock(IPokemonMetadataProvider.class);
+        metadataProvider =new PokemonMetadataProvider();
     }
 
     @Test
@@ -34,5 +34,55 @@ public class IPokemonMetadataProviderTest {
         assertEquals(90, metadata.getStamina());
     }
 
+    @Test
+    public void testMetaDataShouldBeCorrectlyRetrievedBulbizarre() throws PokedexException {
+        // Vérification des informations récupérées pour Bulbizarre
+        PokemonMetadata pokemonMetadata = metadataProvider.getPokemonMetadata(0);
+        assertEquals("Bulbasaur", pokemonMetadata.getName());
+        assertEquals(126, pokemonMetadata.getAttack());
+        assertEquals(126, pokemonMetadata.getDefense());
+        assertEquals(90, pokemonMetadata.getStamina());
+    }
+
+    @Test
+    public void testMetaDataShouldBeCorrectlyRetrievedPikachu() throws PokedexException {
+        PokemonMetadata pokemonMetadata = metadataProvider.getPokemonMetadata(25);
+        assertEquals("Pikachu", pokemonMetadata.getName());
+        assertEquals(112, pokemonMetadata.getAttack());
+        assertEquals(101, pokemonMetadata.getDefense());
+        assertEquals(70, pokemonMetadata.getStamina());
+    }
+
+    @Test
+    public void testMetaDataShouldThrowPokemonException() {
+        // Vérification du lancement de l'exception pour un index invalide (151)
+        assertThrows(PokedexException.class, () -> {
+            metadataProvider.getPokemonMetadata(151);
+        });
+    }
+
+    @Test
+    public void testMetaDataShouldThrowPokemonExceptionNegativeIndex() {
+        // Vérification du lancement de l'exception pour un index négatif (-1)
+        assertThrows(PokedexException.class, () -> {
+            metadataProvider.getPokemonMetadata(-1);
+        });
+    }
+
+    @Test
+    public void testMetaDataShouldThrowPokemonExceptionWhenPokemonNotFound() {
+        // Vérifie le lancement de l'exception pour un Pokémon non présent dans la liste
+        assertThrows(PokedexException.class, () -> {
+            metadataProvider.getPokemonMetadata(10); // Un index qui n'existe pas dans la liste
+        });
+    }
+
+    @Test
+    public void testMetaDataShouldThrowPokemonExceptionWhenInvalidIndex() {
+        // Vérifie le lancement de l'exception pour un index inconnu (par exemple 1000)
+        assertThrows(PokedexException.class, () -> {
+            metadataProvider.getPokemonMetadata(1000);
+        });
+    }
 
 }
