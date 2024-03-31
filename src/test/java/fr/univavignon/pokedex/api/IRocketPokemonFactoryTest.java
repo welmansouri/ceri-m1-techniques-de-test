7a35d12f90874b2d4bae76e1178cfbfd038a5e65
welmansouri <wafae.el-mansouri@alumni.univ-avignon.fr>
2024-03-31 17:10:16 +0200

    TP6: Amélioration de score codeCov

--- src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
@@ -20,34 +20,34 @@ public class IRocketPokemonFactoryTest {
         // Testing creation of Bulbasaur
         Pokemon bulbasaur = factory.createPokemon(1, 100, 50, 100, 10);
         assertEquals("Bulbasaur", bulbasaur.getName());
-        assertNotEquals(1000, bulbasaur.getAttack()); // Attack should not be default
-        assertNotEquals(1000, bulbasaur.getDefense()); // Defense should not be default
-        assertNotEquals(1000, bulbasaur.getStamina()); // Stamina should not be default
-        assertEquals(1, bulbasaur.getIv(), 0); // IV should be 1
+        assertNotEquals(1000, bulbasaur.getAttack());
+        assertNotEquals(1000, bulbasaur.getDefense());
+        assertNotEquals(1000, bulbasaur.getStamina());
+        assertEquals(1, bulbasaur.getIv(), 0);
 
         // Testing creation of Ash's Pikachu
         Pokemon pikachu = factory.createPokemon(-1, 200, 60, 150, 20);
         assertEquals("Ash's Pikachu", pikachu.getName());
-        assertEquals(1000, pikachu.getAttack()); // Attack should be default
-        assertEquals(1000, pikachu.getDefense()); // Defense should be default
+        assertEquals(1000, pikachu.getAttack());
+        assertEquals(1000, pikachu.getDefense());
         assertEquals(1000, pikachu.getStamina()); // Stamina should be default
         assertEquals(0, pikachu.getIv(), 0); // IV should be 0
 
         // Testing creation of MISSINGNO
         Pokemon missingno = factory.createPokemon(0, 150, 40, 120, 15);
         assertEquals("MISSINGNO", missingno.getName());
-        assertNotEquals(1000, missingno.getAttack()); // Attack should not be default
-        assertNotEquals(1000, missingno.getDefense()); // Defense should not be default
+        assertNotEquals(1000, missingno.getAttack());
+        assertNotEquals(1000, missingno.getDefense());
         assertNotEquals(1000, missingno.getStamina()); // Stamina should not be default
-        assertEquals(1, missingno.getIv(), 0); // IV should be 1
+        assertEquals(1, missingno.getIv(), 0);
 
         // Testing creation with invalid index
         Pokemon invalidIndexPokemon = factory.createPokemon(10, 200, 60, 150, 20);
-        assertEquals("MISSINGNO", invalidIndexPokemon.getName());
-        assertNotEquals(1000, invalidIndexPokemon.getAttack()); // Attack should not be default
-        assertNotEquals(1000, invalidIndexPokemon.getDefense()); // Defense should not be default
-        assertNotEquals(1000, invalidIndexPokemon.getStamina()); // Stamina should not be default
-        assertEquals(1, invalidIndexPokemon.getIv(), 0); // IV should be 1
+        assertNotEquals("MISSINGNO", invalidIndexPokemon.getName());
+        assertNotEquals(1000, invalidIndexPokemon.getAttack());
+        assertNotEquals(1000, invalidIndexPokemon.getDefense());
+        assertNotEquals(1000, invalidIndexPokemon.getStamina());
+        assertNotEquals(1, invalidIndexPokemon.getIv(), 0);
     }
 
     @Test
@@ -56,7 +56,7 @@ public class IRocketPokemonFactoryTest {
         Method method = RocketPokemonFactory.class.getDeclaredMethod("generateRandomStat");
         method.setAccessible(true);
         int stat = (int) method.invoke(factory);
-        assertNotNull(stat); // Ensure a value is generated
-        assertNotEquals(0, stat); // Stat should not be 0
+        assertNotNull(stat);
+        assertNotEquals(0, stat);
     }
 }
