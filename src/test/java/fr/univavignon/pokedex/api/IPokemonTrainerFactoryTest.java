40ffc6e2b11b5b6099b6b1c74f288fff08ff01eb
welmansouri <wafae.el-mansouri@alumni.univ-avignon.fr>
2024-03-31 16:45:41 +0200

    TP6: Amélioration de score codeCov

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,77 +1,35 @@
 package fr.univavignon.pokedex.api;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class IPokemonTrainerFactoryTest {
-    private IPokemonTrainerFactory pokemonTrainerFactory;
+    IPokemonTrainerFactory pokemonTrainerFactory = new PokemonTrainerFactory();
+    IPokedexFactory pokedexFactory;
+    IPokedex pokedex;
+    PokemonTrainer pokemonTrainer;
 
-    @BeforeAll
-    public void setUp(){
-        pokemonTrainerFactory = mock(IPokemonTrainerFactory.class);
+    @Before
+    public void setUp() {
+        pokedexFactory = new PokedexFactory();
+        IPokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
+        IPokemonFactory pokemonFactory = new PokemonFactory();
+        pokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+        pokemonTrainer = new PokemonTrainer("Trainer", Team.INSTINCT, pokedex);
     }
 
-
     @Test
-    public void testCreatePokemonTrainer() {
-        Team team = Team.INSTINCT;
-        String trainerName = "Ash";
+    public void shouldGetTrainer() {
+        PokemonTrainer testTrainer = pokemonTrainerFactory.createTrainer("Trainer", Team.INSTINCT, pokedexFactory);
 
-        // Mock des dépendances
-        IPokedexFactory pokedexFactory = mock(IPokedexFactory.class);
-        IPokedex pokedex = mock(IPokedex.class);
-        IPokemonMetadataProvider pokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
-        IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
-
-        // Définition du comportement attendu pour la création du Pokédex
-        when(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory)).thenReturn(pokedex);
-
-        // Définition du comportement attendu pour la création du dresseur
-        when(pokemonTrainerFactory.createTrainer(trainerName, team, pokedexFactory))
-                .thenReturn(new PokemonTrainer(trainerName, team, pokedex));
-
-        // Appel de la méthode de création du dresseur
-        PokemonTrainer pokemonTrainer = pokemonTrainerFactory.createTrainer(trainerName, team, pokedexFactory);
-
-        // Assertions
-        assertNotNull(pokemonTrainer);
-        assertEquals(trainerName, pokemonTrainer.getName());
-        assertEquals(team, pokemonTrainer.getTeam());
-    }
-
-
-    @Test
-    public void testCreateWithNullParams() {
-        Team team = Team.INSTINCT;
-        String trainerName = "Ash";
-
-
-        // Mock des dépendances
-        IPokedexFactory pokedexFactory = mock(IPokedexFactory.class);
-        IPokedex pokedex = mock(IPokedex.class);
-        IPokemonMetadataProvider pokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
-        IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
-
-        // Définition du comportement attendu pour la création du Pokédex
-        when(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory)).thenReturn(pokedex);
-
-        // Définition du comportement attendu pour la création du dresseur avec des paramètres null
-        when(pokemonTrainerFactory.createTrainer(null, team, pokedexFactory)).thenThrow(new IllegalArgumentException());
-        when(pokemonTrainerFactory.createTrainer(trainerName, null, pokedexFactory)).thenThrow(new IllegalArgumentException());
-        when(pokemonTrainerFactory.createTrainer(trainerName, team, null)).thenThrow(new IllegalArgumentException());
-
-        // Assertions pour chaque cas où un paramètre est null
-        assertThrows(IllegalArgumentException.class, () -> pokemonTrainerFactory.createTrainer(null, team, pokedexFactory));
-        assertThrows(IllegalArgumentException.class, () -> pokemonTrainerFactory.createTrainer(trainerName, null, pokedexFactory));
-        assertThrows(IllegalArgumentException.class, () -> pokemonTrainerFactory.createTrainer(trainerName, team, null));
+        assertEquals(pokemonTrainer.getName(), testTrainer.getName());
+        assertEquals(pokemonTrainer.getTeam(), testTrainer.getTeam());
+        assertEquals(pokemonTrainer.getPokedex().size(), testTrainer.getPokedex().size());
+        assertEquals(pokemonTrainer.getPokedex().getPokemons(), testTrainer.getPokedex().getPokemons());
     }
 
 
