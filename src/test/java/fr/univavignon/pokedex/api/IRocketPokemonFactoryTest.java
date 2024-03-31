0a260a96a2237ee82d863aa5a3a9a67a2fb344f5
welmansouri <wafae.el-mansouri@alumni.univ-avignon.fr>
2024-03-31 17:59:54 +0200

    TP6: Amélioration de score codeCov 8

---
+++ src/test/java/fr/univavignon/pokedex/api/IRocketPokemonFactoryTest.java
@@ -0,0 +1,54 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class IRocketPokemonFactoryTest {
+    private IPokemonFactory rocketPokemonFactory;
+    private Pokemon pokemon1;
+
+    @Before
+    public void setUp() throws PokedexException {
+        rocketPokemonFactory = new RocketPokemonFactory();
+    }
+
+    @Test
+    public void testCreatePokemon() throws PokedexException {
+        Pokemon rocket = rocketPokemonFactory.createPokemon(0, 613, 64, 4000, 4);
+        assertEquals(0, rocket.getIndex());
+        assertEquals(613, rocket.getCp());
+        assertEquals(64, rocket.getHp());
+        assertEquals(4000, rocket.getDust());
+        assertEquals(4, rocket.getCandy());
+
+        // Verifier avec un index inferieur à 0
+        Pokemon rocket2 = rocketPokemonFactory.createPokemon(-1, 0, 0, 0, 0);
+
+        assertEquals(rocket2.getIndex(), -1);
+        assertEquals(rocket2.getName(), "Ash's Pikachu");
+        assertEquals(rocket2.getAttack(), 1000);
+        assertEquals(rocket2.getDefense(), 1000);
+        assertEquals(rocket2.getStamina(), 1000);
+        assertEquals(rocket2.getCp(), 0);
+        assertEquals(rocket2.getHp(), 0);
+        assertEquals(rocket2.getDust(), 0);
+        assertEquals(rocket2.getCandy(), 0);
+        assertEquals(rocket2.getIv(), 0, 0);
+
+
+        Pokemon missingno = rocketPokemonFactory.createPokemon(0, 0, 0, 0, 0);
+        assertEquals(missingno.getName(), "MISSINGNO");
+
+        missingno = rocketPokemonFactory.createPokemon(1000, 0, 0, 0, 0);
+        assertEquals(missingno.getName(), "MISSINGNO");
+
+        Pokemon Bulbasaur= rocketPokemonFactory.createPokemon(1, 0, 0, 0, 0);
+        assertEquals(Bulbasaur.getName(), "Bulbasaur");
+
+    }
+
+
+}
